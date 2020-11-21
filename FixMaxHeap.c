336f4f689a50ba2fbd5a3d8b2fdb18088205bfff
WilliamVirtuani <virtuani@gmail.com>
2020-11-21 15:00:37 +0100

    Altri test su funzioni BuildMaxHeap e BuildMinHeap

--- FixMaxHeap.c
+++ FixMaxHeap.c
@@ -5,8 +5,12 @@ void FixMaxHeap(int *A, int root, int right);
 void StampaArray (int *A, int left, int right);
 void FixMaxHeap(int *A, int root, int right);
 void BuildMaxHeap(int *A, int root, int right);
+void BuildMinHeap(int *A, int root, int right);
 int Massimo(int *A, int i, int j); //Ritorna il massimo tra A[i] e A[j]
 int PosizioneMax(int *A, int i, int j);
+int Minimo(int *A, int i, int j);
+int PosizioneMin(int *A, int i, int j);
+void FixMinHeap(int *A, int root, int right);
 
 int main()
 {
@@ -20,7 +24,8 @@ int main()
     scanf("%d", &A[i]);
   }
 
-  BuildMaxHeap(A,0,n-1);
+  BuildMaxHeap(A,3,6);
+  BuildMinHeap(A,0,2);
   StampaArray(A,0,n-1);
   return 0;
 }
@@ -49,10 +54,10 @@ void FixMaxHeap(int *A, int root, int right)
   int sx = 0;
   int pos = 0;
 
-  while((root*2+2) <= right || (root*2+1) <= right) //caso in cui root ha sia foglia destra che sinistra
+  while((root*2+2)-root <= right || (root*2+1)-root <= right) //caso in cui root ha sia foglia destra che sinistra
   {
-    dx = root*2+2;
-    sx = root*2+1;
+    dx = (root*2+2)-root;
+    sx = (root*2+1)-root;
     max = Massimo(A, sx, dx);
     if(sx <= right && dx <= right && A[root] < max)
     {
@@ -75,14 +80,23 @@ void FixMaxHeap(int *A, int root, int right)
 }
 
 
-
 void BuildMaxHeap(int *A, int root, int right)
 {
-  int dim = right+1;
-  int start = ((dim/2)-1);
-  for(int i = start; i >= root; i-- )
+  int dim = (right-root)+1;
+  int start = ((dim/2)-1)+root;
+  for(int i = start; i >= root; i--)
   {
     FixMaxHeap(A, i, right);
+  }
+}
+
+void BuildMinHeap(int *A, int root, int right)
+{
+  int dim = (right-root)+1;
+  int start = ((dim/2)-1)+root;
+  for(int i = start; i >= root; i--)
+  {
+    FixMinHeap(A, i, right);
   }
 }
 
@@ -99,6 +113,18 @@ int Massimo(int *A, int i, int j)
   }
 }
 
+int Minimo(int *A, int i, int j)
+{
+  if(A[i] <= A[j])
+  {
+    return A[i];
+  }
+  else
+  {
+    return A[j];
+  }
+}
+
 int PosizioneMax(int *A, int i, int j)
 {
   if (A[i] >= A[j])
@@ -106,4 +132,45 @@ int PosizioneMax(int *A, int i, int j)
     return i;
   }
   else return j;
+}
+
+int PosizioneMin(int *A, int i, int j)
+{
+  if (A[i] <= A[j])
+  {
+    return i;
+  }
+  else return j;
+}
+
+void FixMinHeap(int *A, int root, int right)
+{
+  int min = 0;
+  int dx = 0;
+  int sx = 0;
+  int pos = 0;
+
+  while((root*2+2)-root <= right || (root*2+1)-root <= right) //caso in cui root ha sia foglia destra che sinistra
+  {
+    dx = (root*2+2)-root;
+    sx = (root*2+1)-root;
+    min = Minimo(A, sx, dx);
+    if(sx <= right && dx <= right && A[root] > min)
+    {
+      pos = PosizioneMin(A, dx, sx);
+      Scambia(A, root, pos);
+      root = pos;
+    }
+    else if(sx > right && A[root] > A[dx] && dx <= right) //caso in cui manca foglia sinistra di root
+    {
+      Scambia(A, root, dx);
+      root = dx;
+    }
+    else if(dx > right && A[root] > A[sx] && sx <= right) //caso in cui manca foglia destra di root
+    {
+      Scambia(A, root, sx);
+      root = sx;
+    }
+    else break;
+  }
 }
