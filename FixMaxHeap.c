9a9614dc77fea230fc7ec5a9f042a2ed22146ee7
WilliamVirtuani <virtuani@gmail.com>
2020-11-23 17:02:02 +0100

    Da Finalizzare e sistemare

--- FixMaxHeap.c
+++ FixMaxHeap.c
@@ -1,33 +1,18 @@
 #include <stdio.h>
 
-void Scambia(int *A, int i, int j); //Scambia due elementi dell'array
-void FixMaxHeap(int *A, int root, int right);
-void StampaArray (int *A, int left, int right);
-void FixMaxHeap(int *A, int root, int right);
+void Scambia(int *A, int i, int j);
 void BuildMaxHeap(int *A, int root, int right);
-void BuildMinHeap(int *A, int root, int right);
-int Massimo(int *A, int i, int j); //Ritorna il massimo tra A[i] e A[j]
-int PosizioneMax(int *A, int i, int j);
-int Minimo(int *A, int i, int j);
-int PosizioneMin(int *A, int i, int j);
-void FixMinHeap(int *A, int root, int right);
+void FixMaxHeap(int *A, int root, int right);
+void StampaArray(int *A, int n);
 
 int main()
 {
-  int n = 0;
-  printf("Inserire dimensione dell'array: ");
-  scanf("%d", &n);
-  int A[n]; //Array
-  printf("Inserire elementi dell'array: ");
-  for(int i = 0; i < n; i++)
-  {
-    scanf("%d", &A[i]);
-  }
-
-  BuildMaxHeap(A,3,6);
-  BuildMinHeap(A,0,2);
-  StampaArray(A,0,n-1);
+  int n = 7;
+  int A[] = {21,1,20,12,15,14,11};
+  FixMaxHeap(A,3,6);
+  //StampaArray(A,n);
   return 0;
+
 }
 
 
@@ -39,138 +24,40 @@ void Scambia(int *A, int i, int j)
   A[j] = buffer;
 }
 
-void StampaArray (int *A, int left, int right)
-{
-  for(int i = left; i < right+1; i++)
-  {
-    printf("%d\t",A[i]);
-  }
-}
-
 void FixMaxHeap(int *A, int root, int right)
 {
   int max = 0;
-  int dx = 0;
   int sx = 0;
+  int dx = 0;
   int pos = 0;
 
-  while((root*2+2)-root <= right || (root*2+1)-root <= right) //caso in cui root ha sia foglia destra che sinistra
+  while(root <= right-1)
   {
-    dx = (root*2+2)-root;
-    sx = (root*2+1)-root;
-    max = Massimo(A, sx, dx);
-    if(sx <= right && dx <= right && A[root] < max)
+    dx = ((root*2)+2)-root;
+    sx = ((root*2)+1)-root;
+    max = root;
+    if(dx <= right && A[dx] > A[max])
     {
-      pos = PosizioneMax(A, dx, sx);
-      Scambia(A, root, pos);
-      root = pos;
+      max = dx;
     }
-    else if(sx > right && A[root] < A[dx] && dx <= right) //caso in cui manca foglia sinistra di root
+   if(sx <= right && A[sx] > A[max])
     {
-      Scambia(A, root, dx);
-      root = dx;
+      max = sx;
     }
-    else if(dx > right && A[root] < A[sx] && sx <= right) //caso in cui manca foglia destra di root
+    if(max != root)
     {
-      Scambia(A, root, sx);
-      root = sx;
+      Scambia(A, root, max);
+      StampaArray(A,7);
+      root = max;
     }
     else break;
   }
 }
 
-
-void BuildMaxHeap(int *A, int root, int right)
+void StampaArray(int *A, int n)
 {
-  int dim = (right-root)+1;
-  int start = ((dim/2)-1)+root;
-  for(int i = start; i >= root; i--)
+  for(int i = 0; i < n; i++)
   {
-    FixMaxHeap(A, i, right);
-  }
-}
-
-void BuildMinHeap(int *A, int root, int right)
-{
-  int dim = (right-root)+1;
-  int start = ((dim/2)-1)+root;
-  for(int i = start; i >= root; i--)
-  {
-    FixMinHeap(A, i, right);
-  }
-}
-
-
-int Massimo(int *A, int i, int j)
-{
-  if(A[i] >= A[j])
-  {
-    return A[i];
-  }
-  else
-  {
-    return A[j];
-  }
-}
-
-int Minimo(int *A, int i, int j)
-{
-  if(A[i] <= A[j])
-  {
-    return A[i];
-  }
-  else
-  {
-    return A[j];
-  }
-}
-
-int PosizioneMax(int *A, int i, int j)
-{
-  if (A[i] >= A[j])
-  {
-    return i;
-  }
-  else return j;
-}
-
-int PosizioneMin(int *A, int i, int j)
-{
-  if (A[i] <= A[j])
-  {
-    return i;
-  }
-  else return j;
-}
-
-void FixMinHeap(int *A, int root, int right)
-{
-  int min = 0;
-  int dx = 0;
-  int sx = 0;
-  int pos = 0;
-
-  while((root*2+2)-root <= right || (root*2+1)-root <= right) //caso in cui root ha sia foglia destra che sinistra
-  {
-    dx = (root*2+2)-root;
-    sx = (root*2+1)-root;
-    min = Minimo(A, sx, dx);
-    if(sx <= right && dx <= right && A[root] > min)
-    {
-      pos = PosizioneMin(A, dx, sx);
-      Scambia(A, root, pos);
-      root = pos;
-    }
-    else if(sx > right && A[root] > A[dx] && dx <= right) //caso in cui manca foglia sinistra di root
-    {
-      Scambia(A, root, dx);
-      root = dx;
-    }
-    else if(dx > right && A[root] > A[sx] && sx <= right) //caso in cui manca foglia destra di root
-    {
-      Scambia(A, root, sx);
-      root = sx;
-    }
-    else break;
+    printf("%d\t", A[i]);
   }
 }
