cab8bbb571dc6d56eef4fc467ba2d9ad470f311b
WilliamVirtuani <virtuani@gmail.com>
2020-11-23 18:47:17 +0100

    Risistemata Funzione FixMaxHeap e aggiunta funzione FixMinHeap

--- FixMaxHeap.c
+++ FixMaxHeap.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 void Scambia(int *A, int i, int j);
-void BuildMaxHeap(int *A, int root, int right);
-void FixMaxHeap(int *A, int root, int right);
+void BuildMaxHeap(int *A, int root, int right, int scale);
+void BuildMinHeap(int *A, int root, int right, int scale);
+void FixMaxHeap(int *A, int root, int right, int scale);
+void FixMinHeap(int *A, int root, int right, int scale);
 void StampaArray(int *A, int n);
 
 int main()
 {
   int n = 7;
-  int A[] = {21,1,20,12,15,14,11};
-  FixMaxHeap(A,3,6);
-  //StampaArray(A,n);
+  int A[] = {21, 1, 20, 12, 15, 14, 11};
+  BuildMinHeap(A,2,5,2);
+  StampaArray(A,n);
   return 0;
 
 }
@@ -24,17 +26,16 @@ void Scambia(int *A, int i, int j)
   A[j] = buffer;
 }
 
-void FixMaxHeap(int *A, int root, int right)
+void FixMaxHeap(int *A, int root, int right, int scale)
 {
   int max = 0;
   int sx = 0;
   int dx = 0;
   int pos = 0;
-
   while(root <= right-1)
   {
-    dx = ((root*2)+2)-root;
-    sx = ((root*2)+1)-root;
+    dx = ((root*2)+2)-scale;
+    sx = ((root*2)+1)-scale;
     max = root;
     if(dx <= right && A[dx] > A[max])
     {
@@ -47,10 +48,57 @@ void FixMaxHeap(int *A, int root, int right)
     if(max != root)
     {
       Scambia(A, root, max);
-      StampaArray(A,7);
       root = max;
     }
     else break;
+  }
+}
+
+void FixMinHeap(int *A, int root, int right, int scale)
+{
+  int max = 0;
+  int sx = 0;
+  int dx = 0;
+  int pos = 0;
+  while(root <= right-1)
+  {
+    dx = ((root*2)+2)-scale;
+    sx = ((root*2)+1)-scale;
+    max = root;
+    if(dx <= right && A[dx] < A[max])
+    {
+      max = dx;
+    }
+   if(sx <= right && A[sx] < A[max])
+    {
+      max = sx;
+    }
+    if(max != root)
+    {
+      Scambia(A, root, max);
+      root = max;
+    }
+    else break;
+  }
+}
+
+void BuildMaxHeap(int *A, int root, int right, int scale)
+{
+  int dim =(right-root)+1;
+  int start =((dim/2)-1)+root;
+  for(int i = start; i >= root; i--)
+  {
+    FixMaxHeap(A, i, right, scale);
+  }
+}
+
+void BuildMinHeap(int *A, int root, int right, int scale)
+{
+  int dim =(right-root)+1;
+  int start =((dim/2)-1)+root;
+  for(int i = start; i >= root; i--)
+  {
+    FixMinHeap(A, i, right, scale);
   }
 }
 
