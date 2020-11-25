ed715ec022572394ba151a2f60919658a435fcd1
WilliamVirtuani <virtuani@gmail.com>
2020-11-26 00:23:56 +0100

    WORK (on small array...)

--- QuickHeapSort.c
+++ QuickHeapSort.c
@@ -9,12 +9,12 @@ void FixMaxHeap(int *A, int root, int right, int scale);
 void FixMinHeap(int *A, int root, int right, int scale);
 int SpecialMaxLeaf(int *A,int left, int right);
 int SpecialMinLeaf(int *A,int left, int right);
-void StampaArray(int *A, int left, int right);
+void StampaArray(int *A, int left, int n);
 
 
 int main()
 {
-  int n = 15;
+  int n = 6;
   int l = 0;
   /*printf("Inserire dimensione dell'array: ");
   scanf("%d", &n);
@@ -24,51 +24,44 @@ int main()
   {
     scanf("%d", &A[i]);
   } */
-  int A[] = {9, 7, 13, 2, 8, 3, 1, 4, 12, 15, 10, 5, 11, 6, 14};
+  int buf;
+  int A[] = {785,5,12,1,4,54};
   int left = 0;
   int right = n-1;
   int m = 0; //Variabile che memorizza la posizione iniziale del pivot
   int pivot = 0; //Variabile che memorizza la posizione finale del pivot
   int pivotEntry = 0; //Variabile che memorizza il pivot
-  //while(left < right)
-  //{
+  while(left < right)
+  {
   m = ChoosePivot(A, left, right);
   pivot = ReversePartition(A, left, right, m);
   pivotEntry = A[pivot];
-  if(pivot <= (n+1)/2)
+  if(right+left > 2*pivot)
   {
     BuildMaxHeap(A, left, pivot-1, left);
-    for(int j = 0; j <= (pivot-left-1); j++)
+    Scambia(A,pivot,right);
+    for(int j = 0; j <= pivot-left-1; j++)
     {
-      StampaArray(A,0,14);
-      printf("\n\n");
       Scambia(A, right-j, left);
-      StampaArray(A,0,14);
-      printf("\n\n");
       l = SpecialMaxLeaf(A, left, pivot-1);
-      StampaArray(A,0,14);
-      printf("\n\n");
       Scambia(A, l, right-j-1);
-      StampaArray(A,0,14);
-      printf("\n\n");
     }
-    Scambia(A, pivot, right-pivot);
     right = right-(pivot-left)-1;
   }
-/*  else
+  else
   {
     BuildMinHeap(A, pivot+1, right, pivot+1);
+    Scambia(A,pivot,left);
     for(int j = 0; j <= right-pivot-1; j++)
     {
       Scambia(A, left+j, pivot+1);
       l = SpecialMinLeaf(A, pivot+1,right);
       Scambia(A, l, left+j+1);
     }
-    Scambia(A,left-1, pivot);
     left = left+(right-pivot)+1;
   }
-}*/
-StampaArray(A,0,14);
+}
+StampaArray(A,0,n);
   return 0;
 }
 
@@ -88,9 +81,9 @@ int ChoosePivot(int *A, int left, int right)
 }
 
 
-void StampaArray (int *A, int left, int right)
+void StampaArray (int *A, int left, int n)
 {
-  for(int i = left; i < right+1; i++)
+  for(int i = left; i < n; i++)
   {
     printf("%d\t",A[i]);
   }
@@ -101,7 +94,7 @@ int ReversePartition(int *A, int left, int right, int m)
 {
   while (left < right)
   {
-    while(A[right] < A[m] )
+    while(A[right] <= A[m])
     {
       right--;
     }
@@ -114,8 +107,8 @@ int ReversePartition(int *A, int left, int right, int m)
       Scambia(A, left, right);
     }
   }
-  Scambia(A, right, m);
-  return right;
+  Scambia(A, left, m);
+  return left;
 }
 
 
@@ -201,7 +194,11 @@ void BuildMinHeap(int *A, int root, int right, int scale)
 
 int SpecialMaxLeaf(int *A,int left, int right)
 {
-  int i = 2*left+1;
+  int i = left+1;
+  if(i > right)
+  {
+    return i-1;
+  }
   while(i < right)
   {
     if(A[i] < A[i+1])
@@ -220,7 +217,11 @@ int SpecialMaxLeaf(int *A,int left, int right)
 }
 int SpecialMinLeaf(int *A,int left, int right)
 {
-  int i = 2*left+1;
+  int i = left+1;
+  if(i > right)
+  {
+    return i-1;
+  }
   while(i < right)
   {
     if(A[i] > A[i+1])
@@ -235,6 +236,7 @@ int SpecialMinLeaf(int *A,int left, int right)
     Scambia(A, (i-1)/2, right);
     i = 2*i+1;
   }
+
   return (i-1)/2;
 
 }
